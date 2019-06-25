ab2bedd8ad0930aa70a5fa9bfff659f6490df9d2
Tanner Folkman <tfolkman33@gmail.com>
2019-06-25 12:34:26 -0400

    running js before scraping and should be able to grab from individual sites

--- web_scraping.py
+++ web_scraping.py
@@ -1,37 +1,87 @@
-import requests
-
 from bs4 import BeautifulSoup
 from csv import writer
 
-import csv
-import sqlite3
+import time
 
-response = requests.get('https://www.xome.com/homes-for-sale/UT/Park-City')
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-soup = BeautifulSoup(response.text, 'html.parser')
+browser = webdriver.Chrome()
 
-properties = soup.find_all(class_='listview-result')
+browser.get("https://www.xome.com/auctions/bank-owned")
+time.sleep(1)
 
-with open('properties.csv', 'w') as csv_file:
-    csv_writer = writer(csv_file)
-    headers = ['Address', 'Price']
-    csv_writer.writerow(headers)
+elem = browser.find_element_by_tag_name("body")
 
-    for property in properties:
-        address = property.find(itemprop='streetAddress').get_text().replace('\n', '').replace('\t', '').replace(' ', '')
-        price = property.find(class_='listview-price').get_text().replace('\n', '')
-        # sqFeet = post.find(class_='listview-address').get_text().replace('\n', ''))
-        csv_writer.writerow([address, price])
+no_of_pagedowns = 15
 
-con = sqlite3.connect("PropertyData.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS Properties (Address, Price);") # use your column names here
+while no_of_pagedowns:
+    elem.send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.2)
+    no_of_pagedowns-=1
 
-with open('properties.csv','rt') as fin: # `with` statement available in 2.5+
-    # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['Address'], i['Price']) for i in dr]
+post_elems = browser.find_elements_by_class_name("property-card-photo")
 
-cur.executemany("INSERT INTO Properties (Address, Price) VALUES (?, ?);", to_db)
-con.commit()
-con.close()
+print(len(post_elems))
+#for post in post_elems:
+#    print(post.text)
+
+#############################################
+
+# from requests_html import HTMLSession
+
+# import HTMLSession from requests_html
+# from requests_html import HTMLSession
+ 
+# # create an HTML Session object
+# session = HTMLSession()
+
+# # Use the object above to connect to needed webpage
+# resp = session.get("https://www.xome.com/auctions/bank-owned")
+ 
+# # Run JavaScript code on webpage
+# resp.html.render(timeout=30)
+
+# import csv
+# import sqlite3
+
+# listings = BeautifulSoup(resp.html.html, 'html.parser')
+# results = listings.find(class_="property-card-photo")
+# # listings = results.find_all(class_="col-md-4 col-sm-6 col-xs-12 sr-property-card-holder")
+
+# # print(resp.status_code)
+# print(len(results))
+#for result in results:
+#    print(listing)
+
+ #with open('properties.csv', 'w') as csv_file:
+    # csv_writer = writer(csv_file)
+    # headers = ['Address', 'City', 'Zip Code', 'State', 'Starting Bid', 
+    # 'Reserve Met', 'Resreve Price', 'Property Type', 'Beds', 'Square Footage', 'Pictures'
+    #  'Property Id', 'URL', 'Auction Start Time', 'Payment Type', 'Listing Agent Info',
+    #   'Event Name', 'Event Details', 'Property Details', 'Listing Information', 'Auction Disclaimers',
+    #    'Price History', 'Tax History', 'Current Market Trends', 'Rental Info']
+
+    # address = listing.find(class_="address-line-1").get_text()
+    # city = listing.find(id="crumb4")
+    # city = city.find("a").get_text()
+    # csv_writer.writerow(headers)
+    # csv_writer.writerow([address, city])
+
+    # for property in properties:
+    #    address = property.find(itemprop='streetAddress').get_text().replace('\n', '').replace('\t', '').replace(' ', '')
+    #    csv_writer.writerow([address, price])
+
+# Tansfer to SQL db
+# con = sqlite3.connect("PropertyData.db")
+# cur = con.cursor()
+# cur.execute("CREATE TABLE IF NOT EXISTS Properties (Address, Price);") # use your column names here
+
+# with open('properties.csv','rt') as fin: # `with` statement available in 2.5+
+#     # csv.DictReader uses first line in file for column headings by default
+#     dr = csv.DictReader(fin) # comma is default delimiter
+#     to_db = [(i['Address'], i['Price']) for i in dr]
+
+# cur.executemany("INSERT INTO Properties (Address, Price) VALUES (?, ?);", to_db)
+# con.commit()
+# con.close()
